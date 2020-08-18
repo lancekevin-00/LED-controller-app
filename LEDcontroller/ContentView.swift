219c448b9ea76c7f5f9c5dafc5264348d0ec127d
Lance Kevin <lancekevin@Lances-MacBook-Air-2.local>
2020-08-18 13:37:15 -0400

    first commit

---
+++ LEDcontroller/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LEDcontroller
+//
+//  Created by Lance Kevin on 8/18/20.
+//  Copyright © 2020 Lance Kevin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
